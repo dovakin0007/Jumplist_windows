df1355b8b7677524204aac326a687e3cf29df109
dovakin0007 <dovakin0007@gmail.com>
2024-07-30 20:13:59 +0530

    please someone fix this crap thanks

---
+++ examples/example.rs
@@ -0,0 +1,120 @@
+use windows::{
+    core::*, Win32::Foundation::*, Win32::Graphics::Gdi::ValidateRect,
+    Win32::System::Com::{COINIT_APARTMENTTHREADED, COINIT_DISABLE_OLE1DDE, CoInitializeEx, CoUninitialize}, Win32::System::LibraryLoader::GetModuleHandleA,
+    Win32::UI::WindowsAndMessaging::*,
+};
+
+use untitled2::*;
+
+fn main() -> Result<()> {
+    unsafe {
+        let instance = GetModuleHandleA(None)?;
+        let window_class = s!("window");
+        let _ = CoInitializeEx(None, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+        let wc = WNDCLASSA {
+            hCursor: LoadCursorW(None, IDC_ARROW)?,
+            hInstance: instance.into(),
+            lpszClassName: window_class,
+
+            style: CS_HREDRAW | CS_VREDRAW,
+            lpfnWndProc: Some(wndproc),
+            ..Default::default()
+        };
+
+        let atom = RegisterClassA(&wc);
+        debug_assert!(atom != 0);
+
+        CreateWindowExA(
+            WINDOW_EX_STYLE::default(),
+            window_class,
+            s!("This is a sample window"),
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            None,
+            None,
+            instance,
+            None,
+        )?;
+
+        let mut message = MSG::default();
+
+        while GetMessageA(&mut message, None, 0, 0).into() {
+            DispatchMessageA(&message);
+        }
+        CoUninitialize();
+        Ok(())
+    }
+}
+
+extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe {
+        match message {
+            WM_CREATE => {
+                println!("works");
+                create_jump_list();
+                LRESULT(0)
+            }
+
+            WM_PAINT => {
+                println!("WM_PAINT");
+                _ = ValidateRect(window, None);
+                LRESULT(0)
+            }
+            WM_DESTROY => {
+                println!("WM_DESTROY");
+                PostQuitMessage(0);
+                LRESULT(0)
+            }
+            _ => DefWindowProcA(window, message, wparam, lparam),
+        }
+    }
+}
+
+unsafe fn create_jump_list() {
+    let mut jump_list = JumpList::new();
+
+    // Creating a custom category
+    let mut custom_category = JumpListCategoryCustom::new(JumpListCategoryType::Custom, Some(String::from("Custom Category")));
+    let args = vec![String::from("arg1"), String::from("arg2")];
+    let mut link1 = JumpListItemLink::new(
+        Some(args.clone()),
+        String::from("Custom Ite 1"),
+        Some(String::from("C:\\Path\\To\\Executable.exe")),
+        Some(String::from("C:\\Path\\To\\Icon.ico")),
+        0,
+    );
+    link1.set_working_dir(String::from("C:\\"));
+    custom_category.jump_list_category.set_visible(true);
+    custom_category.jump_list_category.items.push(Box::new(link1));
+    jump_list.add_category(custom_category);
+
+    // Creating a recent category
+    let mut recent_category = JumpListCategoryCustom::new(JumpListCategoryType::Recent, None);
+    recent_category.jump_list_category.set_visible(true);
+    jump_list.add_category(recent_category);
+
+    // Creating a frequent category
+    let mut frequent_category = JumpListCategoryCustom::new(JumpListCategoryType::Frequent, None);
+    frequent_category.jump_list_category.set_visible(true);
+    jump_list.add_category(frequent_category);
+
+    // Creating a task category
+    let mut task_category = JumpListCategoryCustom::new(JumpListCategoryType::Task, Some(String::from("Tasks")));
+    let mut link2 = JumpListItemLink::new(
+        Some(args.clone()),
+        String::from("Task Item"),
+        Some(String::from("C:\\Path\\To\\Executable2.exe")),
+        None,
+        0,
+    );
+    link2.set_working_dir(String::from("D:\\"));
+    task_category.jump_list_category.set_visible(true);
+    task_category.jump_list_category.items.push(Box::new(link2));
+    jump_list.add_category(task_category);
+
+    jump_list.update();
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
-use std::ffi::c_void;
-use std::ptr::null_mut;
-use windows::core::{GUID, Interface, IUnknown, PROPVARIANT, PWSTR, s};
-use windows::Win32::System::Com::{CLSCTX_INPROC_SERVER, CoCreateInstance, CoInitializeEx};
-use windows::Win32::UI::Shell::{EnumerableObjectCollection, Common::IObjectCollection, IShellLinkW, ShellLink, BHID_PropertyStore, ICustomDestinationList, DestinationList};
+use std::error::Error;
+use windows::core::{GUID, Interface, PROPVARIANT, PWSTR};
+use windows::Win32::System::Com::{CLSCTX_INPROC_SERVER, CoCreateInstance};
+use windows::Win32::UI::Shell::{EnumerableObjectCollection, Common::IObjectCollection, IShellLinkW, ShellLink, ICustomDestinationList, DestinationList, KDC_RECENT, KDC_FREQUENT, IApplicationDestinations, SHAddToRecentDocs, SHARD_SHELLITEM};
 use windows::Win32::UI::Shell::PropertiesSystem::{IPropertyStore, PROPERTYKEY};
-use windows::Win32::Storage::EnhancedStorage::{PKEY_AppUserModel_IsDestListSeparator, PKEY_Title};
+use windows::Win32::Storage::EnhancedStorage::PKEY_Title;
 use windows::Win32::UI::Shell::Common::IObjectArray;
 
+pub fn LOWORD(l: usize) -> usize {
+    l & 0xffff
+}
 
 pub trait JumpListItemTrait {
-    fn get_link(&self) -> IShellLinkW;
+    fn get_link(&self) -> Result<IShellLinkW, Box<dyn Error>>;
 }
 
 #[derive(Default)]
@@ -18,7 +20,7 @@ pub enum JumpListItemType {
     Unknown = 0,
     Link = 1,
     Destination = 2,
-    Separator =3,
+    Separator = 3,
 }
 
 pub struct JumpListItemLink {
@@ -31,15 +33,14 @@ pub struct JumpListItemLink {
     working_dir: Option<String>,
 }
 
-
 pub fn to_w_str(str: String) -> Vec<u16> {
-    let mut new_str=str.encode_utf16().collect::<Vec<u16>>();
+    let mut new_str = str.encode_utf16().collect::<Vec<u16>>();
     new_str.push(0);
     new_str
 }
 
 impl JumpListItemLink {
-    pub fn new(command_args: Option<Vec<String>>, title: String, command: Option<String>, icon: Option<String>, icon_index:i32) -> Self {
+    pub fn new(command_args: Option<Vec<String>>, title: String, command: Option<String>, icon: Option<String>, icon_index: i32) -> Self {
         Self {
             list_type: JumpListItemType::Link,
             command_args,
@@ -51,7 +52,7 @@ impl JumpListItemLink {
         }
     }
 
-    pub fn get_working_dir(&self) -> &Option<String>{
+    pub fn get_working_dir(&self) -> &Option<String> {
         &self.working_dir
     }
 
@@ -61,94 +62,77 @@ impl JumpListItemLink {
 }
 
 impl JumpListItemTrait for JumpListItemLink {
-    fn get_link(&self) -> IShellLinkW {
-
+    fn get_link(&self) -> Result<IShellLinkW, Box<dyn Error>> {
         let shell_link_guid: *const GUID = &ShellLink;
-        let link: IShellLinkW= unsafe { CoCreateInstance(shell_link_guid, None, CLSCTX_INPROC_SERVER) }.unwrap();
-        let mut command =to_w_str(self.command.clone().unwrap());
-        let command_pstr:PWSTR = PWSTR(command.as_mut_ptr());
-        unsafe { link.SetPath(command_pstr).unwrap(); }
-        let mut command_line_vec =self.command_args.clone().unwrap().iter_mut().map( |string| format!(" \"{}\"", string.replace('"', "\\\"")) ).collect::<Vec<String>>();
-        let mut command_line_string = String::new();
-        let _ = command_line_vec.iter_mut().map( |str|  {
-            command_line_string.push_str(&str.clone()[..]);
-        });
-        if !command_line_vec.is_empty() {
-            let command_line_string_pwstr:PWSTR = PWSTR(to_w_str( command_line_string.clone()).as_mut_ptr());
-            unsafe { link.SetArguments(command_line_string_pwstr).unwrap() };
-        }
-        if !self.working_dir.is_none() {
-            let wd_pwstr = PWSTR(to_w_str(self.working_dir.clone().unwrap()).as_mut_ptr());
-            unsafe { link.SetWorkingDirectory(wd_pwstr).unwrap()};
-        }
-        if !self.icon.is_none() {
-            let icon_pwstr = PWSTR(to_w_str(self.icon.clone().unwrap()).as_mut_ptr());
-            unsafe { link.SetIconLocation(icon_pwstr, self.icon_index).unwrap() };
-        }
-        println!("issue");
-        // let property_store_guid: *const GUID = &BHID_PropertyStore;
-        let properties = link.cast::<IPropertyStore>().unwrap();
+        let link: IShellLinkW = unsafe { CoCreateInstance(shell_link_guid, None, CLSCTX_INPROC_SERVER)? };
 
-        let property_store: IPropertyStore = properties ;
-        let p_title_key: *const PROPERTYKEY = &PKEY_Title ;
+        if let Some(command) = &self.command {
+            let command_pwstr = PWSTR(to_w_str(command.clone()).as_mut_ptr());
+            unsafe { link.SetPath(command_pwstr)?; }
+        }
 
+        if let Some(args) = &self.command_args {
+            let command_line = args.iter().map(|arg| format!(" \"{}\"", arg.replace('"', "\\\""))).collect::<String>();
+            let command_line_pwstr = PWSTR(to_w_str(command_line).as_mut_ptr());
+            unsafe { link.SetArguments(command_line_pwstr)?; }
+        }
+
+        if let Some(wd) = &self.working_dir {
+            let wd_pwstr = PWSTR(to_w_str(wd.clone()).as_mut_ptr());
+            unsafe { link.SetWorkingDirectory(wd_pwstr)?; }
+        }
+
+        if let Some(icon) = &self.icon {
+            let icon_pwstr = PWSTR(to_w_str(icon.clone()).as_mut_ptr());
+            unsafe { link.SetIconLocation(icon_pwstr, self.icon_index)?; }
+        }
+
+        let properties = link.cast::<IPropertyStore>()?;
+        let property_store: IPropertyStore = properties;
+        let p_title_key: *const PROPERTYKEY = &PKEY_Title;
         let p_variant: *const PROPVARIANT = &PROPVARIANT::from(self.title.as_str());
-        unsafe { property_store.SetValue(p_title_key, p_variant).unwrap(); }
-        unsafe { property_store.Commit().unwrap(); }
+        unsafe { property_store.SetValue(p_title_key, p_variant)?; }
+        unsafe { property_store.Commit()?; }
 
-        link
-
+        Ok(link)
     }
 }
-
 
 pub struct JumpListItemSeparator {
-    list_type: JumpListItemType
+    list_type: JumpListItemType,
 }
 
-impl  JumpListItemSeparator {
+impl JumpListItemSeparator {
     pub fn new() -> Self {
         Self {
-            list_type: JumpListItemType::Separator
+            list_type: JumpListItemType::Separator,
         }
     }
-
 }
 
-
 impl JumpListItemTrait for JumpListItemSeparator {
-    fn get_link(&self) -> IShellLinkW {
+    fn get_link(&self) -> Result<IShellLinkW, Box<dyn Error>> {
         let shell_link_guid: *const GUID = &ShellLink;
+        let link: IShellLinkW = unsafe { CoCreateInstance(shell_link_guid, None, CLSCTX_INPROC_SERVER)? };
 
-        let link: IShellLinkW = unsafe { CoCreateInstance(shell_link_guid, None, CLSCTX_INPROC_SERVER) }.unwrap();
-        let property_store_guid: *const GUID = &BHID_PropertyStore;
-        let mut properties: *mut *mut c_void = null_mut();
-
-        unsafe { link.query(property_store_guid, properties).unwrap() };
-        let property_store: *mut IPropertyStore = properties as *mut IPropertyStore;
-        let p_title_key: *const PROPERTYKEY = &PKEY_AppUserModel_IsDestListSeparator;
-
-        unsafe {
-            if let Some(property_store_back) = property_store.as_ref() {
-                let p_variant: *const PROPVARIANT = &PROPVARIANT::from(true);
-                property_store_back.SetValue(p_title_key, p_variant).unwrap();
-                property_store_back.Commit().unwrap();
-                link
-            } else {
-                panic!();
-            }
-        }
+        let properties = link.cast::<IPropertyStore>()?;
+        let property_store: IPropertyStore = properties;
+        let p_title_key: *const PROPERTYKEY = &PKEY_Title;
+        let p_variant: *const PROPVARIANT = &PROPVARIANT::from(true);
+        unsafe { property_store.SetValue(p_title_key, p_variant)?; }
+        unsafe { property_store.Commit()?; }
+        Ok(link)
     }
 }
 
 pub enum JumpListCategoryType {
     Custom = 0,
     Task = 1,
-    Recent =2,
+    Recent = 2,
     Frequent = 3,
 }
 
-pub struct JumpListCategory{
+pub struct JumpListCategory {
     pub jl_category_type: JumpListCategoryType,
     pub items: Vec<Box<dyn JumpListItemTrait>>,
     visible: bool,
@@ -163,16 +147,26 @@ impl JumpListCategory {
         }
     }
 
-    pub unsafe fn get_category(&self) -> IObjectCollection {
-        let obj_collection:*const GUID =  &EnumerableObjectCollection;
-        let clsctx_inproc = windows::Win32::System::Com::CLSCTX_INPROC_SERVER;
-        let collection:IObjectCollection  = CoCreateInstance(obj_collection, None, clsctx_inproc).unwrap();
-        for i in &self.items {
+    pub unsafe fn get_category(&mut self) -> Result<IObjectCollection, Box<dyn Error>> {
+        let obj_collection: *const GUID = &EnumerableObjectCollection;
+        let collection: IObjectCollection = CoCreateInstance(obj_collection, None, CLSCTX_INPROC_SERVER)?;
 
-            collection.AddObject(&i.get_link()).unwrap();
-            println!("are u even working");
+        let mut items_to_remove = vec![];
+
+        for (index, item) in self.items.iter().enumerate() {
+            if let Ok(link) = item.get_link() {
+                collection.AddObject(&link)?;
+            } else {
+                items_to_remove.push(index);
+            }
         }
-        collection
+
+        // Remove items that failed to create links
+        for index in items_to_remove.iter().rev() {
+            self.items.remove(*index);
+        }
+
+        Ok(collection)
     }
 
     pub fn get_visible(&self) -> bool {
@@ -180,24 +174,50 @@ impl JumpListCategory {
     }
 
     pub fn set_visible(&mut self, visible: bool) {
-        self.visible = true
+        self.visible = visible;
     }
 }
 
-
-
 pub struct JumpListCategoryCustom {
     pub jump_list_category: JumpListCategory,
-    pub title: String
+    pub title: Option<String>,
 }
 
 impl JumpListCategoryCustom {
-    pub fn new(title: String) -> Self {
-        let mut jl_category = JumpListCategory::new();
-        jl_category.jl_category_type = JumpListCategoryType::Custom;
-        Self {
-            jump_list_category: jl_category,
-            title
+    pub fn new(jl_type: JumpListCategoryType, title: Option<String>) -> Self {
+        match jl_type {
+            JumpListCategoryType::Custom => {
+                let mut jl_category = JumpListCategory::new();
+                jl_category.jl_category_type = JumpListCategoryType::Custom;
+                Self {
+                    jump_list_category: jl_category,
+                    title,
+                }
+            }
+            JumpListCategoryType::Recent => {
+                let mut jl_category = JumpListCategory::new();
+                jl_category.jl_category_type = JumpListCategoryType::Recent;
+                Self {
+                    jump_list_category: jl_category,
+                    title: None,
+                }
+            }
+            JumpListCategoryType::Frequent => {
+                let mut jl_category = JumpListCategory::new();
+                jl_category.jl_category_type = JumpListCategoryType::Frequent;
+                Self {
+                    jump_list_category: jl_category,
+                    title: None,
+                }
+            }
+            JumpListCategoryType::Task => {
+                let mut jl_category = JumpListCategory::new();
+                jl_category.jl_category_type = JumpListCategoryType::Task;
+                Self {
+                    jump_list_category: jl_category,
+                    title: None,
+                }
+            }
         }
     }
 }
@@ -205,15 +225,12 @@ impl JumpListCategoryCustom {
 pub struct JumpList {
     jumplist: ICustomDestinationList,
     task: JumpListCategory,
-    pub custom: Vec<JumpListCategoryCustom>
+    pub custom: Vec<JumpListCategoryCustom>,
 }
 
-impl  JumpList {
-
-
+impl JumpList {
     pub unsafe fn new() -> Self {
-
-        let dest_list:*const GUID=&DestinationList;
+        let dest_list: *const GUID = &DestinationList;
         let jumplist: ICustomDestinationList = CoCreateInstance(dest_list, None, CLSCTX_INPROC_SERVER).unwrap();
         Self {
             jumplist,
@@ -221,40 +238,87 @@ impl  JumpList {
             custom: vec![],
         }
     }
+
     pub fn get_tasks(&mut self) -> &JumpListCategory {
         &self.task
     }
 
-    pub unsafe fn update(&self) {
-        //let min_slot:*mut u32 = ;
-
-        &self.jumplist.BeginList::<IObjectArray>( &mut 10).unwrap();
-        let is_command_empty = &self.task.items.is_empty().clone();
-
-
-        if self.task.visible & is_command_empty {
-            println!("works2");
-            &self.jumplist.AddUserTasks(&self.task.get_category()).unwrap();
-        }
-
-        for category in &self.custom {
-            let x = category.jump_list_category.items.is_empty();
-            if category.jump_list_category.visible & !x{
-                let title = to_w_str(category.title.clone());
-                println!("worked");
-                &self.jumplist.AppendCategory(PWSTR(title.clone().as_mut_ptr()), &category.jump_list_category.get_category()).unwrap();
+    pub unsafe fn update(&mut self) {
+        self.jumplist.BeginList::<IObjectArray>(&mut 10).unwrap();
+        let is_command_empty = self.task.items.is_empty();
+        if self.task.visible && !is_command_empty {
+            if let Ok(value) = self.task.get_category() {
+                if let Err(err) = self.jumplist.AddUserTasks(&value) {
+                    eprintln!("Error adding user tasks: {:?}", err);
+                }
             }
         }
-        &self.jumplist.CommitList();
+
+        let mut categories_to_remove = vec![];
+
+        for (index, category) in self.custom.iter_mut().enumerate() {
+            match category.jump_list_category.jl_category_type {
+                JumpListCategoryType::Custom => {
+                    if category.jump_list_category.visible && !category.jump_list_category.items.is_empty() {
+                        if let Some(title) = &category.title {
+                            let title_wstr = to_w_str(title.clone());
+                            if let Ok(value) = category.jump_list_category.get_category() {
+                                if let Err(err) = self.jumplist.AppendCategory(PWSTR(title_wstr.as_ptr() as *mut u16), &value) {
+                                    eprintln!("Error appending custom category: {:?}", err);
+                                    categories_to_remove.push(index);
+                                }
+                            }
+                        }
+                    }
+                }
+                JumpListCategoryType::Recent => {
+                    if category.jump_list_category.visible && !category.jump_list_category.items.is_empty() {
+                        if let Err(err) = self.jumplist.AppendKnownCategory(KDC_RECENT) {
+                            eprintln!("Error appending recent category: {:?}", err);
+                            categories_to_remove.push(index);
+                        }
+                    }
+                }
+                JumpListCategoryType::Frequent => {
+                    if category.jump_list_category.visible && !category.jump_list_category.items.is_empty() {
+                        if let Err(err) = self.jumplist.AppendKnownCategory(KDC_FREQUENT) {
+                            eprintln!("Error appending frequent category: {:?}", err);
+                            categories_to_remove.push(index);
+                        }
+                    }
+                }
+                JumpListCategoryType::Task => {
+                    if category.jump_list_category.visible && !category.jump_list_category.items.is_empty() {
+                        if let Ok(value) = category.jump_list_category.get_category() {
+                            if let Err(err) = self.jumplist.AddUserTasks(&value) {
+                                eprintln!("Error adding user tasks: {:?}", err);
+                                categories_to_remove.push(index);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Remove categories that failed to append
+        for index in categories_to_remove.iter().rev() {
+            println!("worked");
+            self.custom.remove(*index);
+        }
+
+        if let Err(err) = self.jumplist.CommitList() {
+            eprintln!("Error committing jump list: {:?}", err);
+        }
     }
 
-    pub unsafe fn delete_list(&self, appid: String) {
-        self.jumplist.SetAppID(PWSTR(to_w_str(appid.clone()).clone().as_mut_ptr())).unwrap();
-        &self.jumplist.DeleteList(PWSTR(to_w_str(appid.clone()).clone().as_mut_ptr())).unwrap();
+    pub unsafe fn delete_list(&self) {
+        if let Err(err) = self.jumplist.DeleteList(None) {
+            eprintln!("Error deleting jump list: {:?}", err);
+        }
     }
-    pub unsafe fn add_category(&mut self,  category: JumpListCategoryCustom) {
-        &self.custom.push(category);
+
+    pub unsafe fn add_category(&mut self, category: JumpListCategoryCustom) {
+        self.custom.push(category);
     }
 }
-
 
